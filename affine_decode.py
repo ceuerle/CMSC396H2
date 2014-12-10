aa68c5832eb0a549a71524c47d161dbb08aeff5b
Eric Harrison <ejharriso@gmail.com>
2014-12-10 02:15:37 -0500

    final

--- affine_decode.py
+++
@@ -1,9 +0,0 @@
-# Input: An affine-encoded text
-# Output: Most probable affine-decoded message of text
-
-import sys
-from ciphers import arr_to_str, str_to_arr, encode, affine_decode
-
-message = str_to_arr(sys.stdin.read()[:-1])
-
-print arr_to_str(encode(message, 1, affine_decode(message)))
--- shift_decode.py
+++
@@ -1,9 +0,0 @@
-# Input: Encoded text
-# Output: Most probable shift-decoded message of text
-
-import sys
-from ciphers import str_to_arr, arr_to_str, encode, shift_decode
-
-message = str_to_arr(sys.stdin.read()[:-1])
-
-print arr_to_str(encode(message, 1, shift_decode(message)))
